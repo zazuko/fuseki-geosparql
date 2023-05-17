efd2bc18429ab0ba427ccb4defb190f5387f37ab
Ludovic Muller <ludovicmuller1@gmail.com>
2023-05-17 18:54:34 +0200

    test: test if GeoSPARQL is working

--- tests/run.sh
+++ tests/run.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-DOCKER_BUILDKIT=1 docker-compose up -d --build
+DOCKER_BUILDKIT=1 docker-compose build
+DOCKER_BUILDKIT=1 docker-compose up -d
 sleep 10
 hurl --test *.hurl
 docker-compose down
