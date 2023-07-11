344e361a80d0ad24e527f6f93a9781b959f5b399
Ludovic Muller <ludovicmuller1@gmail.com>
2023-07-11 15:31:24 +0200

    ci: just inform about the wait time

--- tests/run.sh
+++ tests/run.sh
@@ -5,6 +5,7 @@ set -eu
 docker-compose pull
 DOCKER_BUILDKIT=1 docker-compose build
 DOCKER_BUILDKIT=1 docker-compose up -d
+echo "Waiting 20 seconds…"
 sleep 20
 hurl --test *.hurl
 docker-compose down
